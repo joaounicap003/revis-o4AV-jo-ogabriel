decb875ea07c31a2a02b8186fdd538c8bfe3e5a1
aluno <aluno0@unicap.br>
2024-06-07 09:14:00 -0300

    questão 1

---
+++ questao01.py
@@ -0,0 +1,11 @@
+def maior(*numeros):
+    maior_elemento = numeros[0]
+    for i in range(len(numeros)):
+        if numeros[i] > maior_elemento:
+            maior_elemento = numeros[i]
+            
+    print(maior_elemento)
+
+maior(1,2,3,4,5)
+maior(4,7)
+maior(100,1)
